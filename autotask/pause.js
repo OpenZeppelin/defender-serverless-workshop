79dd540a32c209a73192cd58c04271e26c194ac0
Robert Leonard <robertleonard@Roberts-MacBook-Pro-2.local>
2022-11-03 14:12:58 -0400

    Autotask to automatically pause workshop contract for a given address

--- autotask/pause.js
+++ autotask/pause.js
@@ -1,17 +1,14 @@
-const { Relayer } = require('defender-relay-client');
+const { DefenderRelayProvider, DefenderRelaySigner } = require('defender-relay-client/lib/ethers');
+const { ethers } = require('ethers');
 
-const abi = [{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"attackInProgress","type":"event"},{"inputs":[{"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"pauseContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"vulnerableFunction","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+const abi = [{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"attackInProgress","type":"event"},{"inputs":[{"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"pauseContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"workshopperAddress","type":"address"}],"name":"vulnerableFunction","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+const ADDRESS = "0x238C14b8FE864596B0bb7cce134A7c8b8C7692a1";
 
 exports.handler = async function(params) {
-   const client = new Relayer(params);
-    const relayer = await client.getRelayer();
-    const provider = new DefenderRelayProvider(params);
-    const signer = new DefenderRelaySigner(params, provider, {
-      speed: 'fast',
-      from: relayer.address,
-    });
-    const WorkshopContract = new ethers.Contract(address, abi, signer);
-
-  	console.log(JSON.stringify(params.request.body));
-    return await WorkshopContract.pauseContract(params.request.body.workshopperAddress)
+   const provider = new DefenderRelayProvider(params);
+   const signer = new DefenderRelaySigner(params, provider, { speed: 'fast' });
+   const WorkshopContract = new ethers.Contract(ADDRESS, abi, signer);
+   const workshopAddress = params.request.body.matchReasons[0].args[0];
+   await WorkshopContract.pauseContract(workshopAddress);
+  return `Paused contract for address: ${workshopAddress}`;
 }
